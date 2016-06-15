58fbff31b0d1a17d9cb6d03f903a90b9106b1824
Bartosz Bonislawski <boniek0210@gmail.com>
2016-06-16 00:07:40 +0200

    add authentication users

--- sklepmuzyczny/urls.py
+++ sklepmuzyczny/urls.py
@@ -1,6 +1,17 @@
 from django.conf.urls import url
+from django.contrib.auth.views import login, logout
+from django.views.generic.edit import CreateView
+from django.contrib.auth.forms import UserCreationForm
 from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+	url(r'^accounts/login/$',  login),
+    url(r'^accounts/register/', CreateView.as_view(
+        template_name='registration.html',
+        form_class=UserCreationForm,
+        success_url='/'
+    )),
+   url(r'^accounts/logout/$', logout),
+    url(r'^discs/(?P<disc_id>[0-9]+)/$', views.detail, name='disc-detail'),
 ]
--- sklepmuzyczny/views.py
+++ sklepmuzyczny/views.py
@@ -1,7 +1,28 @@
 from django.shortcuts import render
 from .models import Disc
 from .models import Cart
+from django.contrib import auth
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.http import HttpResponseRedirect
+
 
 def index(request):
 	discs = Disc.objects.all()
 	return render(request, 'sklepmuzyczny/discs_list.html', {'discs': discs})
+
+def detail(request, disc_id):
+	disc = Disc.objects.get(pk=disc_id)
+	return render(request, 'sklepmuzyczny/disc_show.html', {'disc': disc})
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            new_user = form.save()
+            return HttpResponseRedirect("/")
+    else:
+        form = UserCreationForm()
+    return render(request, "registration/register.html", {
+        'form': form,
+    })
