5195a4ebdd781225ae95d3871ac39dc61aa0e8b5
Bartosz Bonislawski <boniek0210@gmail.com>
2016-06-15 21:27:19 +0200

    add first template and view

--- sklepmuzyczny/views.py
+++ sklepmuzyczny/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+	return render(request, 'sklepmuzyczny/discs_list.html', )
+    # return HttpResponse("Hello, world. You're at the polls index.")
 # def discs_list(request):
 # 	return render(request, 'sklepmuzyczny/discs_list.html', {})
 # Create your views here.
