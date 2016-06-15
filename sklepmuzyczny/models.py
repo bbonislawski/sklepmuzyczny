5b033c879787a097db513f97d601f2ae8c448faa
Bartosz Bonislawski <boniek0210@gmail.com>
2016-06-16 00:54:37 +0200

    little changes in models

--- sklepmuzyczny/models.py
+++ sklepmuzyczny/models.py
@@ -2,12 +2,12 @@ from django.db import models
 from django.utils import timezone
 
 class Cart(models.Model):
-    author = models.ForeignKey('auth.User')
+    user = models.ForeignKey('auth.User')
     created_date = models.DateTimeField(
             default=timezone.now)
     
     def __str__(self):
-        return "Koszyk uzytkownika:" + author 
+        return "Koszyk uzytkownika:" + self.user.username
         
 class Disc(models.Model):
     author = models.CharField(max_length=200)
