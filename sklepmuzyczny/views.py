7a06f056b8e3eb67ee9b5b6780476e2a90aae421
Bartosz Bonislawski <boniek0210@gmail.com>
2016-06-15 22:03:42 +0200

    add bootstrap theme

--- sklepmuzyczny/views.py
+++ sklepmuzyczny/views.py
@@ -1,9 +1,7 @@
 from django.shortcuts import render
+from .models import Disc
+from .models import Cart
 
-from django.http import HttpResponse
 def index(request):
-	return render(request, 'sklepmuzyczny/discs_list.html', )
-    # return HttpResponse("Hello, world. You're at the polls index.")
-# def discs_list(request):
-# 	return render(request, 'sklepmuzyczny/discs_list.html', {})
-# Create your views here.
+	discs = Disc.objects.all()
+	return render(request, 'sklepmuzyczny/discs_list.html', {'discs': discs})
